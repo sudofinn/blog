022408bcdca3147dab1aca36fc3bbf5979d15d7f
sudofinn <gufi127127@gmail.com>
2022-07-10 20:29:19 +0200

    Setup back to initial commit, basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {ChakraProvider} from "@chakra-ui/react"
-import {useState, useEffect} from "react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-   const [showChild, setShowChild] = useState(false);
-  useEffect(() => {
-    setShowChild(true);
-  }, []);
-
-  if (!showChild) {
-    return null;
-  }
-
-  if (typeof window === 'undefined') {
-    return <></>;
-  } else {
+function MyApp({ Component, pageProps }: AppProps) { 
   return  (
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
 
   )
-  }
 }
+
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,12 @@ import Head from "next/head"
 
 import styles from '../styles/Home.module.css'
 
-import HomePageUi from '../components/ui/HomePageUi'
 
 
 const Home: NextPage = () => {
   return (
     <div>
-    <HomePageUi />
+      <h1>Hello World</h1>
 
     </div>
   )
