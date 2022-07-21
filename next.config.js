a3fcb59d4e2abb8e6d90d6131676fb6d22dadbc0
sudofinn <gufi127127@gmail.com>
2022-07-21 12:16:05 +0200

    W. .env.local all working good

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    getFormKey:"https://getform.io/f/e1b4940b-ffd6-466a-889d-08d3deaf0298"
-  }
+  
 }
 
 module.exports = nextConfig
